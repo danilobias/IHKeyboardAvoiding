601459d8ddc1d309c62862e7e36d132868195eee
Danilo Bias <danilobias@hotmail.com>
2023-10-28 16:48:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,11 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
-
+let name = "IHKeyboardAvoiding"
 let package = Package(
-    name: "IHKeyboardAvoiding",
+    name: name,
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "IHKeyboardAvoiding",
-            targets: ["IHKeyboardAvoiding"]),
+        .library(name: "IHKeyboardAvoiding", targets: ["IHKeyboardAvoiding"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "IHKeyboardAvoiding"),
-        .testTarget(
-            name: "IHKeyboardAvoidingTests",
-            dependencies: ["IHKeyboardAvoiding"]),
+        .target(name: "IHKeyboardAvoiding", path: "IHKeyboardAvoiding"),
     ]
 )
